0d721c4e3d77e5f8aa70b2c5fac77ff7d664ed0f
Yegor Kazantsev <wwwadmin@bk.ru>
2023-03-25 20:29:29 +0300

    explore.py и Readme

--- explore.py
+++ explore.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
-import os
+from pprint import pprint
 
+import requests
 from dotenv import load_dotenv
 
-from repositories.csv_list import CsvImageListRepository
 from repositories.settings import Settings
 from ya_disk.auth import auth
-from ya_disk.list import get_list
 from ya_disk.token import get_token
 from ya_disk.token import has_authtoken
 
 load_dotenv()
+
+
+def get_ya_disk_info(settings: dict, token: str) -> dict:
+    headers = {"Accept": "application/json", "Authorization": f"OAuth {token}"}
+    url = "https://cloud-api.yandex.net/v1/disk/"
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+def get_ya_trash_resources(settings: dict, token: str) -> dict:
+    headers = {"Accept": "application/json", "Authorization": f"OAuth {token}"}
+    url = "https://cloud-api.yandex.net/v1/disk/trash/resources/"
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
 def main():
     settings: dict = Settings.get_settings()
     if not settings["YANDEX_CLIENT_ID"]:
@@ -19,9 +34,10 @@ def main():
     if not has_authtoken():
         auth(settings)
     token = get_token()
-    result = get_list(settings["YA_DISK_ROOT"], token, settings)
-    repo = CsvImageListRepository(settings=settings)
-    repo.save_image_list(result)
+    result = get_ya_disk_info(settings, token)
+    pprint(result)
+    result = get_ya_trash_resources(settings, token)
+    pprint(result)
 
 
 if __name__ == "__main__":
